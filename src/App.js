9468a95e5a5e851f0aa7dadb2e2aed082b937da0
abraidotti <alessandro.braidotti@gmail.com>
2019-12-30 15:28:01 -0500

    add backend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { Component } from "react";
+import { withAuthenticator } from "aws-amplify-react";
 
-function App() {
-  return (
-   <div>App</div>
-  );
+class App extends Component {
+  state = {
+    notes: [
+      {
+        id: 1,
+        note: "Hello world"
+      }
+    ]
+  };
+
+  render() {
+    const { notes } = this.state;
+
+    return (
+      <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
+        <h1 className="code f2-l">Amplify Notetaker</h1>
+        {/*Note form*/}
+        <form className="mb3">
+          <input type="text" className="pa2 f4" placeholder="write your note" />
+          <button className="pa2 f4" type="submit">
+            Add Note
+          </button>
+        </form>
+
+        {/* notes list */}
+
+        {notes.map(item => (
+          <div key={item.id} className="flex items-center">
+            <li className="list pa1 f3">{item.note}</li>
+            <button className="bg-transparent bn f4">
+              <span>&times;</span>
+            </button>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
